b1e7993848b3eb2e2db30cdd858a71fba428ffb6
Myakita <146758985+Myakita@users.noreply.github.com>
2025-04-25 14:39:26 +0500

    Add files via upload

---
+++ C#/13 лаба/Program.cs
@@ -0,0 +1,46 @@
+﻿using ЛабораторнаяРабота12;
+using ShipLibrary;
+
+namespace Лабораторная_работа_13
+{
+    internal class Program : ShipCollectionNew<string, Ship>
+    {
+        static void Main(string[] args)
+        {
+            ShipCollectionNew<string, Ship> FTable = new ShipCollectionNew<string, Ship>() { Name = "FTable" };
+            ShipCollectionNew<string, Ship> STable = new ShipCollectionNew<string, Ship>() { Name = "STable" };
+
+
+            Journal FJournal = new Journal();
+            Journal SJournal = new Journal();
+
+            FTable.CollectionCountChanged += FJournal.CollectionCountChanged;
+            FTable.CollectionReferenceChanged += FJournal.CollectionRefChanged;
+
+            STable.CollectionReferenceChanged += SJournal.CollectionRefChanged;
+            STable.CollectionCountChanged += SJournal.CollectionCountChanged;
+
+            FTable.Add("Корабль1", new Ship("Корабль1", 2));
+            FTable.Add("Корабль2", new Corvette("Корабль2", 2, 3));
+            FTable.Add("Корабль3", new Steamship("Корабль3", 2, 4));
+
+            STable.Add("Корабль4", new Ship("Корабль4", 2));
+            STable.Add("Корабль5", new Steamship("Корабль5", 2, 4));
+            STable.Add("Корабль6", new Sailboat("Корабль6", 2, 52));
+
+            FTable.Remove("Корабль1");
+            FTable.Remove("Корабль3");
+
+            STable.Remove("Корабль4");
+            STable.Remove("Корабль6");
+
+            FTable["Корабль2"] = new Corvette("КорабльТесея", 2, 42);
+            STable["Корабль5"] = new Steamship("ЕщеКорабльТесея", 3, 55);
+
+            Console.WriteLine("Первый журнал:");
+            FJournal.PrintLog();
+            Console.WriteLine("Второй журнал:");
+            SJournal.PrintLog();
+        }
+    }
+}
---
+++ C#/13 лаба/ShipCollectionNew.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ЛабораторнаяРабота12;
+using ShipLibrary;
+
+namespace Лабораторная_работа_13
+{
+    public class ShipCollectionNew<TKey, TValue> : ShipHashTable<TKey, TValue>
+    {
+        public string Name { get; set; }
+
+        public delegate void CollectionHandler(object source, CollectionHandlerEventArgs args);
+
+        public event CollectionHandler CollectionCountChanged;
+        public event CollectionHandler CollectionReferenceChanged;
+
+        public void Add(TKey key, TValue value)
+        {
+            base.Add(key, value);
+            OnCollectionCountChanged(new CollectionHandlerEventArgs(Name, "Добавление", value));
+        }
+
+        public bool Remove(TKey key)
+        {
+            if (base.ContainsKey(key))
+            {
+                TValue removedShip = this[key];
+                bool removed = base.Remove(key);
+                if (removed)
+                {
+                    OnCollectionCountChanged(new CollectionHandlerEventArgs(Name, "Удаление", removedShip));
+                }
+                return removed;
+            }
+            return false;
+        }
+
+        public TValue this[TKey key]
+        {
+            get => base[key];
+            set
+            {
+                if (base.ContainsKey(key))
+                {
+                    OnCollectionReferenceChanged(new CollectionHandlerEventArgs(Name, "Изменение ссылки", value));
+                }
+                base[key] = value;
+            }
+        }
+
+        public virtual void OnCollectionCountChanged(CollectionHandlerEventArgs args)
+        {
+            CollectionCountChanged?.Invoke(this, args);
+        }
+
+        public virtual void OnCollectionReferenceChanged(CollectionHandlerEventArgs args)
+        {
+            CollectionReferenceChanged?.Invoke(this, args);
+        }
+
+        public class CollectionHandlerEventArgs : EventArgs
+        {
+            public string Name { get; }
+            public string Action { get; }
+            public object AffObject { get; }
+
+            public CollectionHandlerEventArgs(string name, string action, object affobject)
+            {
+                Name = name;
+                Action = action;
+                AffObject = affobject;
+            }
+
+            public override string ToString()
+            {
+                return $"Коллекция:{Name}, Действие:{Action}, Что:{AffObject}";
+            }
+
+        }
+
+
+        public class Journal
+        {
+            public List<string> log = new List<string>();
+
+            public void CollectionCountChanged(object source, CollectionHandlerEventArgs ev)
+            {
+                log.Add(ev.ToString());
+            }
+            public void CollectionRefChanged(object source, CollectionHandlerEventArgs ev)
+            {
+                log.Add(ev.ToString());
+            }
+
+            public void PrintLog()
+            {
+                foreach (var item in log)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
